01625d1bc1fa43080f0c2c8f0214e30c37b7560a
Aman-Shetty <amanshetty370@gmail.com>
2023-10-31 21:03:59 +0530

    Warehouse Page update

--- src/pages/Warehouse/WareHouse.jsx
+++ src/pages/Warehouse/WareHouse.jsx
@@ -1,4 +1,23 @@
+import { Footer, NavBar } from "../../components";
+import WarehouseCard from "./components/WarehouseCard";
+
 const WareHouse = () => {
-  return <div>WareHouse</div>;
+  return (
+    <div>
+      <NavBar />
+      <div className="m-8 flex flex-wrap">
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+        <WarehouseCard className="w-fit" />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 export default WareHouse;
